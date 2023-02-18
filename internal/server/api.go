edf2ef767a4dc7dc677b3fdf2af1c73053296587
Nicole Kistler <njrockower@gmail.com>
2023-02-18 15:42:49 -0500

    Start working on operations + more mock data

--- internal/server/api.go
+++ internal/server/api.go
@@ -1,8 +1,11 @@
 package server
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func (s *Server) registerApiRoutes() {
@@ -13,15 +16,28 @@ func (s *Server) registerApiRoutes() {
 
 // Get all game rooms
 func (s *Server) getRooms(w http.ResponseWriter, r *http.Request) {
-	log.Println(s.Rooms)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(s.Rooms)
+}
+
+// Get a game room by ID
+func (s *Server) getRoom(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for _, item := range s.Rooms {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+
+	log.Println("Bruh")
+
+	json.NewEncoder(w).Encode(Room{})
 }
 
 // Create a game room
 func (s *Server) createRoom(w http.ResponseWriter, r *http.Request) {
 	log.Println("Create room")
-}
-
-// Get a game room by ID
-func (s *Server) getRoom(w http.ResponseWriter, r *http.Request) {
-	log.Println("Get room")
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -8,6 +8,54 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+func NewServer() *Server {
+	serv := &Server{
+		Rooms: []Room{
+			{
+				ID:       "1",
+				State:    RoomState{Round: 0},
+				Settings: Settings{MaxNumberRounds: 10},
+				Users: []User{
+					{
+						ID:       "1",
+						Name:     "Stella",
+						AvatarID: "4",
+						IsHost:   true,
+						TeamId:   "1",
+					},
+				},
+			},
+			{
+				ID:       "2",
+				State:    RoomState{Round: 0},
+				Settings: Settings{MaxNumberRounds: 7},
+				Users: []User{
+					{
+						ID:       "4",
+						Name:     "Possum",
+						AvatarID: "3",
+						IsHost:   true,
+						TeamId:   "2",
+					},
+					{
+						ID:       "6",
+						Name:     "Rila",
+						AvatarID: "7",
+						IsHost:   true,
+						TeamId:   "2",
+					},
+				},
+			},
+		},
+		Upgrader: &websocket.Upgrader{
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+		},
+	}
+
+	return serv
+}
+
 func (s *Server) Init() error {
 	s.Router = mux.NewRouter().StrictSlash(true)
 
@@ -21,21 +69,4 @@ func (s *Server) Init() error {
 	}
 
 	return nil
-}
-
-func NewServer() *Server {
-	serv := &Server{
-		Rooms: []Room{
-			{
-				ID:    "1",
-				State: RoomState{Round: 0},
-			},
-		},
-		Upgrader: &websocket.Upgrader{
-			ReadBufferSize:  1024,
-			WriteBufferSize: 1024,
-		},
-	}
-
-	return serv
 }
